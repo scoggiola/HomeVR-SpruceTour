65d818250d5598dfb399477bce9bc85c1c02e2f8
Dylan Ross Ludwick <dludwick@bimaire.com>
2018-02-19 15:04:54 -0700

    change elevation ext from png to jpg

--- home-vr-demo/public/js/main.js
+++ home-vr-demo/public/js/main.js
@@ -119,7 +119,7 @@ $(document).ready(() => {
     function handleCaseElevation(elevation, color) {
       currentSrc = imgSrc;
       concatSrc(elevation);
-      capSrc('.png');
+      capSrc('.jpg');
       updateImg(anchors.base, imgSrc);
       imgSrc = currentSrc;
       if (color !== options.color[0]) {
