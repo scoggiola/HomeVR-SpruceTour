b5fa6c3db4c38072cf15f44b6461c8050d5e432b
Dylan Ross Ludwick <dludwick@bimaire.com>
2018-02-23 14:34:30 -0700

    cleanup

--- client/ClientVR/components/BtnboxOverlay.js
+++ client/ClientVR/components/BtnboxOverlay.js
@@ -3,12 +3,11 @@ import exitIcon from '../static_assets/icons/exit.png';
 
 // Gets props via the DomOverlayModule and uses those to render content
 const BtnboxOverlay = (props) => {
-  console.log(exitIcon);
   return (
     <div className='overlay'>
       <div className='btn-container-a'>
         <div className='btn-a' onClick={ () => props.handleClick('exitVR button') }>
-          
+
         </div>
       </div>
       <div className='btn-container-b'>
